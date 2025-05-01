34dcd1dc4d5ebae1af12feb243c8015f0ee4bbd0
Bill Chung <koohong@berkeley.edu>
2025-05-01 11:00:23 -0700

    second_commit

--- source/get_lib.R
+++ source/get_lib.R
@@ -1,4 +1,24 @@
-list.of.packages <- c("ggplot2", "Rcpp")
+list.of.packages <- c("Rcpp","tidyverse","ggplot2", ,
+                      "MASS","rpart","glmnet","GGally","patchwork",
+                      "data.table","readr","readxl","ggridge")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
+
+'%!in%' <- function(x,y)!('%in%'(x,y))
+
+library(tidyverse)
+library(patchwork)
+library(ggplot2)
+library(GGally)
+library(ggridges)
+library(plotly)
+
+library(readr)
+library(readxl)
+library(data.table) #fread
+
+library(MASS)
+library(glmnet)
+
+library(lubridate)
